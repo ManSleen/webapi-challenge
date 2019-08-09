7b95be005fd2b19b4082a148ee73c1a22c9b7f8b
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-09 12:31:37 -0600

    Added route for /

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require("./server");
 
 const port = process.env.PORT || 8090;
 
+server.get("/", (req, res) => {
+  res.send("Hello world, its Mike! Here's my sprint challenge!");
+});
+
 server.listen(port, () => {
   console.log(`\nAPI is running strong on port ${port}\n`);
 });
